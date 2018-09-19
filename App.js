e30e414e95263ac626da2dd7a80bbe73c9c73ab7
Yuki Matsushita <ymatsushita@picniic.com>
2018-09-18 22:50:47 -0700

    update 2-1

--- App.js
+++ App.js
@@ -1,26 +1,29 @@
-import React from 'react'; // reactライブラリのインポート
-import { StyleSheet, Text, View } from 'react-native'; // react nativeのコンポーネントのインポート
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
-// クラスの定義
-// export defautl: モジュールごとに1つだけ。importする際に特に指定がなければそのクラスや関数を呼ぶ
-// React.Component: React.Componentの継承。クラス内でのreactの利用
 export default class App extends React.Component {
-  render() { // reactのライフサイクル。描画する
+  render() {
     return (
-      <View style={styles.container}> {/* react nativeのViewコンポーネント */}
-        <Text>Open up App.js to start working on your app!</Text>{/* react nativeのTextコンポーネント */}
-      </View>// 閉じタグが必要
+      <View style={styles.container}>
+        <Image style={styles.image} resizeMode="contain" source={require("./assets/kaiun.png")} />
+        <Text style={styles.text}>今日の運勢は？</Text>
+      </View>
     );
   }
 }
 
-// スタイルシートオブジェクトの作成
-// StyleSheet.createでオブジェクトIdを返す
 const styles = StyleSheet.create({
   container: {
-    flex: 1, // flexboxを利かす
-    backgroundColor: '#fff',
-    alignItems: 'center', // 水平方向に中央寄せ
-    justifyContent: 'center', // 垂直方向に中央寄せ
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  text: {
+    fontSize: 40
+  },
+  image: {
+    width: 200,
+    height: 50
+  }
 });
