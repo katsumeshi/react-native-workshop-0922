a767a16c054d657545bb14a250a539c39b39c87e
Yuki Matsushita <ymatsushita@picniic.com>
2018-09-19 20:47:25 -0700

    update 2-1

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Image style={styles.image} resizeMode="contain" source={require("./assets/kaiun.png")} />
-        <Text style={styles.text}>今日の運勢は？</Text>
+        <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
@@ -18,12 +17,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
-  },
-  text: {
-    fontSize: 40
-  },
-  image: {
-    width: 200,
-    height: 50
   }
 });
