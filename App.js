28214a306e04616a674327bd7d3ccb0533d598e2
Yuki Matsushita <ymatsushita@picniic.com>
2018-09-19 21:32:52 -0700

    update 2_2

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default class App extends React.Component {
-  handleOmikuji = () => {
-    alert("test");
-  };
-
   render() {
     return (
       <View style={styles.container}>
         <Image style={styles.image} resizeMode="contain" source={require("./assets/kaiun.png")} />
         <Text style={styles.text}>今日の運勢は？</Text>
-        <Image style={styles.resultImage} resizeMode="contain" source={require("./assets/daikichi.png")} />
-        <TouchableOpacity style={styles.buttonContainer} onPress={this.handleOmikuji}>
-          <Text style={styles.buttonTitle}>おみくじを引く</Text>
-        </TouchableOpacity>
       </View>
     );
   }
@@ -33,18 +25,5 @@ const styles = StyleSheet.create({
   image: {
     width: 200,
     height: 50
-  },
-  resultImage: {
-    width: 200,
-    height: 300
-  },
-  buttonContainer: {
-    backgroundColor: "#3498db"
-  },
-  buttonTitle: {
-    fontSize: 25,
-    color: "white",
-    paddingHorizontal: 40,
-    paddingVertical: 10
   }
 });
