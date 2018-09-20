c5fd726e28fa0de53da25d2096c4ac74b6ea9917
Yuki Matsushita <ymatsushita@picniic.com>
2018-09-19 21:52:58 -0700

    updaate 3_3

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import React from "react";
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 
+const options = [require("./assets/daikichi.png"), require("./assets/chukichi.png"), require("./assets/shokichi.png"), require("./assets/suekichi.png"), require("./assets/daikyo.png")];
+
 export default class App extends React.Component {
+  state = {
+    image: null
+  };
+
   handleOmikuji = () => {
-    alert("大吉！！！");
+    const index = Math.floor(Math.random() * options.length);
+    this.setState({
+      image: options[index]
+    });
   };
 
   render() {
+    const { image } = this.state;
     return (
       <View style={styles.container}>
         <Image style={styles.image} resizeMode="contain" source={require("./assets/kaiun.png")} />
         <Text style={styles.text}>今日の運勢は？</Text>
-        <Image style={styles.resultImage} resizeMode="contain" source={require("./assets/daikichi.png")} />
+        <Image style={styles.resultImage} resizeMode="contain" source={image} />
         <TouchableOpacity style={styles.buttonContainer} onPress={this.handleOmikuji}>
           <Text style={styles.buttonTitle}>おみくじを引く</Text>
         </TouchableOpacity>
@@ -25,7 +35,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "space-around"
   },
   text: {
     fontSize: 40
