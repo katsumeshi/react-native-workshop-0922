77cb290a6fe9c76a66ae3345d26c05ebf39fa33b
Yuki Matsushita <ymatsushita@picniic.com>
2018-09-20 20:45:58 -0700

    add navigation work

--- App.js
+++ App.js
@@ -1,60 +1,12 @@
-import React from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import Omikuji from "./screens/omikuji";
+import Result from "./screens/result";
+import { createStackNavigator } from "react-navigation";
 
-const options = [require("./assets/daikichi.png"), require("./assets/chukichi.png"), require("./assets/shokichi.png"), require("./assets/suekichi.png"), require("./assets/daikyo.png")];
-
-export default class App extends React.Component {
-  state = {
-    image: null
-  };
-
-  handleOmikuji = () => {
-    const index = Math.floor(Math.random() * options.length);
-    this.setState({
-      image: options[index]
-    });
-  };
-
-  render() {
-    const { image } = this.state;
-    return (
-      <View style={styles.container}>
-        <Image style={styles.image} resizeMode="contain" source={require("./assets/kaiun.png")} />
-        <Text style={styles.text}>今日の運勢は？</Text>
-        <Image style={styles.resultImage} resizeMode="contain" source={image} />
-        <TouchableOpacity style={styles.buttonContainer} onPress={this.handleOmikuji}>
-          <Text style={styles.buttonTitle}>おみくじを引く</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "space-around"
+export default createStackNavigator({
+  Omikuji: {
+    screen: Omikuji
   },
-  text: {
-    fontSize: 40
-  },
-  image: {
-    width: 200,
-    height: 50
-  },
-  resultImage: {
-    width: 200,
-    height: 300
-  },
-  buttonContainer: {
-    backgroundColor: "#3498db"
-  },
-  buttonTitle: {
-    fontSize: 25,
-    color: "white",
-    paddingHorizontal: 40,
-    paddingVertical: 10
+  Result: {
+    screen: Result
   }
 });
---
+++ screens/omikuji.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity, Button } from "react-native";
+
+const options = [require("../assets/daikichi.png"), require("../assets/chukichi.png"), require("../assets/shokichi.png"), require("../assets/suekichi.png"), require("../assets/daikyo.png")];
+
+export default class Omikuji extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: "おみくじ",
+      headerRight: <Button onPress={() => navigation.navigate("Result")} title="結果一覧" color="black" />
+    };
+  };
+
+  state = {
+    image: null
+  };
+
+  handleOmikuji = () => {
+    const index = Math.floor(Math.random() * options.length);
+    this.setState({
+      image: options[index]
+    });
+  };
+
+  render() {
+    const { image } = this.state;
+    return (
+      <View style={styles.container}>
+        <Image style={styles.image} resizeMode="contain" source={require("../assets/kaiun.png")} />
+        <Text style={styles.text}>今日の運勢は？</Text>
+        <Image style={styles.resultImage} resizeMode="contain" source={image} />
+        <TouchableOpacity style={styles.buttonContainer} onPress={this.handleOmikuji}>
+          <Text style={styles.buttonTitle}>おみくじを引く</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-around"
+  },
+  text: {
+    fontSize: 40
+  },
+  image: {
+    width: 200,
+    height: 50
+  },
+  resultImage: {
+    width: 200,
+    height: 300
+  },
+  buttonContainer: {
+    backgroundColor: "#3498db"
+  },
+  buttonTitle: {
+    fontSize: 25,
+    color: "white",
+    paddingHorizontal: 40,
+    paddingVertical: 10
+  }
+});
